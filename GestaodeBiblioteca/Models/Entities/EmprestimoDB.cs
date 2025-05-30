3ed4f2ef2aecce77edc687333cde5a27349b708c
Igor Acatauassu <igoracatauassu03@gmail.com>
2025-05-30 00:00:51 -0300

    Adicionando mais features

--- GestaodeBiblioteca/Models/Entities/EmprestimoDB.cs
+++ GestaodeBiblioteca/Models/Entities/EmprestimoDB.cs
@@ -5,40 +5,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaodeBiblioteca.Models.Entities
 {
+    [Table("emprestimo")]
     public class EmprestimoDB
     {
         [Key]
+        [Column("id")]
         public int Id { get; set; }
 
         [Required]
+        [Column("id_usuario")]
         [ForeignKey("Usuario")]
         public int IdUsuario { get; set; }
 
-        public UsuarioDB Usuario { get; set; }  
+        public UsuarioDB Usuario { get; set; } 
 
         [Required]
+        [Column("id_livro")]
         [ForeignKey("Livro")]
         public int IdLivro { get; set; }
 
         public LivroDB Livro { get; set; } 
 
         [Required]
+        [Column("data_emprestimo")]
         public DateTime DataEmprestimo { get; set; } = DateTime.Now;
 
         [Required]
+        [Column("data_devolucao_prevista")]
         public DateTime DataDevolucaoPrevista { get; set; }
 
+        [Column("data_devolucao_efetiva")]
         public DateTime? DataDevolucaoEfetiva { get; set; }
 
         [Required]
-        [EnumDataType(typeof(StatusEmprestimo))]
-        public StatusEmprestimo Status { get; set; } = StatusEmprestimo.Ativo;
+        [Column("status")]
+        public string Status { get; set; }
+
     }
 
-    public enum StatusEmprestimo
-    {
-        Ativo,
-        Finalizado,
-        Atrasado
-    }
+
 }
